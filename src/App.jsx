ea7f1e81f56e17e731fe076c15f1be30f0de852e
Carl-Johan <CarlJohank@hotmail.com>
2024-11-07 17:06:08 +0100

    bug fixex

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   const [cyferOutput, setCyferOutput] = useState("your input")
 
   function setOutput(cryptMetod){
-    const output = document.querySelector('#test-input').value;
-    const key = document.querySelector('#text-input').value;
+    const output = document.querySelector('#text-input').value;
+    const key = document.querySelector('#key-input').value;
     setCyferOutput(cryptMetod + ": " + output + " key: " + key)
   }
 
